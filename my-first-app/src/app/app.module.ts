bdad23f5c83f5c6996ba7e5672c8993ad4c7c5c6
Garrett Glanz <garrettglanz77@gmail.com>
2021-12-07 00:44:15 -0600

    updates to first app

--- my-first-app/src/app/app.module.ts
+++ my-first-app/src/app/app.module.ts
@@ -1,17 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+
+
 import { AppComponent } from './app.component';
-import { ReactiveFormsModule } from '@angular/forms';
+import { ServerComponent } from './server/server.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent
   ],
   imports: [
-    BrowserModule, 
+    BrowserModule,
     FormsModule,
-    ReactiveFormsModule
+    
+
     
   ],
   providers: [],
