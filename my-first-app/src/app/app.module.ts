2a35544c7a16c011231069164f5b5fa66547510f
Garrett Glanz <garrettglanz77@gmail.com>
2021-12-09 00:45:49 -0600

    Practice making components manually and with CLI, created a warning/success component and styled them.

--- my-first-app/src/app/app.module.ts
+++ my-first-app/src/app/app.module.ts
@@ -1,20 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-
+//import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
-import { ServerComponent } from './server/server.component';
-import { ServersComponent } from './servers/servers.component';
+//import { ServerComponent } from './server/server.component';
+//import { ServersComponent } from './servers/servers.component';
+import { SuccessAlertComponent } from './success-alert/success-alert.component';
+import { WarningAlertComponent } from './warning-alert/warning-alert.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ServerComponent,
-    ServersComponent
+    //ServerComponent,
+    //ServersComponent,
+    SuccessAlertComponent,
+    WarningAlertComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
+    //HttpModule
     
     
 
