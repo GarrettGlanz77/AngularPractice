57adf2cd706f7228c2a628addc5bcfcfcaa9c2d1
Garrett Glanz <garrettglanz77@gmail.com>
2021-12-22 15:20:27 -0500

    practiced more data binding and string interpolation

--- my-first-app/src/app/server/server.component.ts
+++ my-first-app/src/app/server/server.component.ts
@@ -5,5 +5,6 @@ import { Component } from "@angular/core";
     templateUrl: './server.component.html'
 })
 export class ServerComponent {
-
+    serverId: number =  10;
+    serverStatus:  string = 'offline';
 }
